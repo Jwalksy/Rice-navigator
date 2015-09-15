793264674a6292b6e4d72962aacc4980bb8337e5
Jwalksy <chiheng2013@gmail.com>
2015-09-15 17:09:47 -0500

    added rice navigator

---
+++ Hack/location2.js
@@ -0,0 +1,38 @@
+<option value="29.719307,-95.402432">Keith-Wiess Geological Laboratories(KWG)</option>
+
+<option value="29.717909,-95.402367">Ley Student Center(LEY)</option>
+
+<option value="29.716558,-95.398044">Lovett College, Edgar Odell(LVC)</option>
+
+<option value="29.717565,-95.403168">McNair Hall, Janice and Robert(MCN)</option>
+
+<option value="29.72017,-95.400333">Mechanical Engineering Building(MEB)</option>
+
+<option value="29.720901,-95.400038">Mechanical Laboratory(MEL)</option>
+
+<option value="29.713674,-95.405695">Media Center(MED)</option>
+
+<option value="29.722112,-95.397689">Martel College, Marian and Speros R.(MLC)</option>
+
+<option value="29.720448,-95.401159">Mudd Computer Science Building(MUD)</option>
+
+<option value="29.721883,-95.396546">North Servery(NSV)</option>
+
+<option value="29.721128,-95.401309">Oshman Engineering Design Kitchen(OED)</option>
+
+<option value="29.718022,-95.399035">Rayzor Hall(RZR)</option>
+
+<option value="29.718372,-95.397861">Sewall Hall(SEW)</option>
+
+<option value="29.719879,-95.402536">Space Science and Technology Building(SST)</option>
+
+<option value="29.715484,-95.401024">South Servery(SSV)</option>
+
+<option value="29.716289,-95.409318">Rice Stadium(STA)</option>
+
+<option value="29.715102,-95.404138">Tudor Fieldhouse and Youngkin Center(TUD)</option>
+
+<option value="29.714992,-95.400661">Wiess College, Harry C.(WSC)</option>
+
+<option value="29.721063,-95.398481">West Servery(WSV)</option>
+
---
+++ Hack/others/nearby.js
@@ -0,0 +1,66 @@
+var geocoder;
+var map;
+var infowindow;
+var pyrmont = new google.maps.LatLng(-33.8665433, 151.1956316);
+
+function initialize() {  
+  var latlng = new google.maps.LatLng(-34.397, 150.644);
+  var mapOptions = {
+    zoom: 8,
+    center: latlng
+  }
+
+ map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+ codeAddress();
+
+  var request = {
+    location: pyrmont,
+    radius: 500,
+    types: ['store']
+  };
+
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
+}
+
+
+function codeAddress() {
+  geocoder = new google.maps.Geocoder();  
+  var address = '100 Murray St, Pyrmont NSW, Australia';   
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
